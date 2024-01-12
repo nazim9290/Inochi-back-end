aeb49e7a02ef40873aa56591dd63c2638e463740
arefin5 <arefintalukder5@gmail.com>
2024-01-12 21:54:45 +0600

    role change route

--- controlar/userAuth.js
+++ controlar/userAuth.js
@@ -224,7 +224,34 @@ exports.allFree = async (req, res) => {
     res.status(500).send('Internal Server Error');
   }
 };
+// user role change by admin
 
+exports.userRole = async (req, res) => {
+  const userId = req.params.id;
+
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Update user information
+    user.role = req.body.updatedRole; // Make sure to send the updated value from React
+
+    // Save the updated user document
+    await user.save();
+
+    // Exclude the password from the response
+    user.password = undefined;
+
+    // Send a response indicating success
+    res.json({ message: 'Profile updated successfully', user });
+  } catch (error) {
+    console.error('Error updating profile:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 exports.uploadImage = async (req, res) => {
   // console.log("req files => ",req.files);
   try {
--- route/userRoute.js
+++ route/userRoute.js
@@ -1,25 +1,26 @@
-const  express =require("express")
-const {requireAuth} =require("../midleware/auth")
+const express = require("express")
+const { requireAuth } = require("../midleware/auth")
 const router = express.Router();
-const {checkAdmin}=require("../midleware/admin") ;
-const {checkStudent}=require("../midleware/student.js")
+const { checkAdmin } = require("../midleware/admin");
+const { checkStudent } = require("../midleware/student.js")
 // controllers
-const formidable=require("express-formidable")
-const {register,login,currentUser, createStudentdetails, getAllStudents,
-  upDateProfile,uploadImage
-,allFree
-}=require("../controlar/userAuth.js");
+const formidable = require("express-formidable")
+const { register, login, currentUser, createStudentdetails, getAllStudents,
+  upDateProfile, uploadImage
+  , allFree, userRole
+} = require("../controlar/userAuth.js");
 router.post(
   "/upload-image-file",
   formidable({ maxFileSize: 5 * 1024 * 1024 }),
   uploadImage
 );
 
-router.post("/register",register);
-router.post("/login",login);
+router.post("/register", register);
+router.post("/login", login);
 router.get('/profile', requireAuth, currentUser);
-router.post("/create-student/:id",requireAuth,checkAdmin,createStudentdetails);
-router.get("/all-student",requireAuth,checkAdmin,getAllStudents);
-router.get("/all-guset",requireAuth,checkAdmin,allFree)
-router.put("/profile-update/:id",upDateProfile)
+router.post("/create-student/:id", requireAuth, checkAdmin, createStudentdetails);
+router.get("/all-student", requireAuth, checkAdmin, getAllStudents);
+router.get("/all-guset", requireAuth, checkAdmin, allFree)
+router.put("/profile-update/:id", upDateProfile);
+router.put("change-role/:id", requireAuth, checkAdmin, userRole)
 module.exports = router;
