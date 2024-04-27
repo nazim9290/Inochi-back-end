c943d28969e30e65bba8a8194618cd6524ac8560
arefin5 <arefintalukder5@gmail.com>
2024-04-27 14:39:08 +0600

    change data type

--- models/contactModel.js
+++ models/contactModel.js
@@ -11,7 +11,7 @@ const contactSchema = new mongoose.Schema({
   },
   phone: {
     type: String,
-    required: true,
+    // required: true,
   },
   msg: {
     type: String,
--- route/userRoute.js
+++ route/userRoute.js
@@ -7,7 +7,7 @@ const { checkStudent } = require("../midleware/student.js")
 const formidable = require("express-formidable")
 const { register, login, currentUser, createStudentdetails, getAllStudents,
   upDateProfile, uploadImage
-  , allFree, userRole,AllStudent,
+  , allFree, userRole, AllStudent,
   BookSeminer,
   getBranchA,
   getBranchB,
@@ -21,7 +21,7 @@ router.post(
 
 router.post("/register", register);
 // router.post("/seminer-book",BookSeminer)
-router.post("/seminer-book",BookSeminer)
+router.post("/seminer-book", BookSeminer)
 
 router.post("/login", login);
 router.get('/profile', requireAuth, currentUser);
@@ -31,7 +31,7 @@ router.get("/all-guset", requireAuth, checkAdmin, allFree);
 router.get("/all-students", requireAuth, checkAdmin, AllStudent);
 router.get("/all-students/brancha", requireAuth, checkAdmin, getBranchA);
 router.get("/all-students/branchb", requireAuth, checkAdmin, getBranchB);
-router.get("/all-seminer-booking",BookSeminerGet)
+router.get("/all-seminer-booking", BookSeminerGet)
 router.put("/profile-update/:id", upDateProfile);
 router.put("/change-role/:id", requireAuth, checkAdmin, userRole);
 module.exports = router;
